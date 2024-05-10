fdc9b5c1319c887f9d91e335eee36ff6719349b4
hechtbaptist <hechtbaptist@gmail.com>
2024-05-10 12:24:03 +0000

    update cookiedomain&secure

--- src/contexts/AuthKitProvider.tsx
+++ src/contexts/AuthKitProvider.tsx
@@ -7,8 +7,8 @@ import AuthProvider from "react-auth-kit/AuthProvider";
 const store = createStore({
     authName: "__auth",
     authType: "cookie",
-    cookieDomain: process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
-    cookieSecure: false,
+    cookieDomain: window.location.hostname,
+    cookieSecure: window.location.protocol === "https:",
 });
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
